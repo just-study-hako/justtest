1886e4fe36cd8261803ddedab7b0d1fb6a3ada44
mine <mineki94@naver.com>
2024-04-01 11:42:10 +0900

    테스트 해보기 파일 생성

---
+++ src/test/java/com/example/justtest/JusttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.justtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JusttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
